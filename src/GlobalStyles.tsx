70278094d23476db6aac2aaabef0bae0a586fbe7
Bear_bear <unegbuclinton5@gmail.com>
2022-07-23 20:33:40 +0100

    create work enviornment

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+import { COLORS } from './constants/colors';
+const GlobalStyle = createGlobalStyle`
+    *, *::before, *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    html {
+        font-size: 62.5%;
+    }
+
+    body {
+        color: inherit;
+        background: ${COLORS.black};
+    }
+    ::-webkit-scrollbar-thumb {
+        border-radius: 5px;
+    }
+    ::-webkit-scrollbar {
+       display: none;
+    }
+    ::-webkit-scrollbar-track {
+    }
+`;
+
+export default GlobalStyle;
